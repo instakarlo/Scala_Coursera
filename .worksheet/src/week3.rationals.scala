f3bf0f6c6817b61d712c21fee2dd63517e09b555
instakarlo <cyclops_101ph@yahoo.com>
2018-06-08 00:27:05 +0800

    Added simplification of rationals, less, max and require

--- .worksheet/src/week3.rationals.scala
+++ .worksheet/src/week3.rationals.scala
@@ -1,10 +1,7 @@
 package week3
 
-object rationals {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(62); 
-  val x = new Rational(1, 2);System.out.println("""x  : week3.Rational = """ + $show(x ));$skip(10); val res$0 = 
-  x.numer;System.out.println("""res0: Int = """ + $show(res$0));$skip(10); val res$1 = 
-  x.denom;System.out.println("""res1: Int = """ + $show(res$1));$skip(143); 
-  
+object rationals {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(174); 
+
   def addRational(a: Rational, b: Rational): Rational = {
   	new Rational((a.numer * b.denom) + (a.denom * b.numer), a.denom * b.denom)
   };System.out.println("""addRational: (a: week3.Rational, b: week3.Rational)week3.Rational""");$skip(76); 
@@ -13,26 +10,57 @@ object rationals {;import org.scalaide.worksheet.runtime.library.WorksheetSuppor
   	a.numer + "/" + a.denom
   };System.out.println("""makeString: (a: week3.Rational)String""");$skip(65); 
   
-  var s = addRational(new Rational(1, 2), new Rational(1, 2));System.out.println("""s  : week3.Rational = """ + $show(s ));$skip(10); val res$2 = 
-  s.numer;System.out.println("""res2: Int = """ + $show(res$2));$skip(10); val res$3 = 
-  s.denom;System.out.println("""res3: Int = """ + $show(res$3));$skip(66); val res$4 = 
-  makeString(addRational(new Rational(1, 2), new Rational(2, 3)));System.out.println("""res4: String = """ + $show(res$4));$skip(143); val res$5 = 
-                                                  
-                                                  
-  x.add(new Rational(2, 3)).toStringV2();System.out.println("""res5: String = """ + $show(res$5));$skip(37); 
-  
-  
+  var s = addRational(new Rational(1, 2), new Rational(1, 2));System.out.println("""s  : week3.Rational = """ + $show(s ));$skip(10); val res$0 = 
+  s.numer;System.out.println("""res0: Int = """ + $show(res$0));$skip(10); val res$1 = 
+  s.denom;System.out.println("""res1: Int = """ + $show(res$1));$skip(66); val res$2 = 
+  makeString(addRational(new Rational(1, 2), new Rational(2, 3)));System.out.println("""res2: String = """ + $show(res$2));$skip(30); 
+
+  val x = new Rational(1, 2);System.out.println("""x  : week3.Rational = """ + $show(x ));$skip(10); val res$3 = 
+  x.numer;System.out.println("""res3: Int = """ + $show(res$3));$skip(10); val res$4 = 
+  x.denom;System.out.println("""res4: Int = """ + $show(res$4));$skip(39); val res$5 = 
+  x.add(new Rational(2, 3)).toStringV2;System.out.println("""res5: String = """ + $show(res$5));$skip(31); 
   
   val a = new Rational(1,3);System.out.println("""a  : week3.Rational = """ + $show(a ));$skip(28); 
   val b = new Rational(5,7);System.out.println("""b  : week3.Rational = """ + $show(b ));$skip(28); 
   val c = new Rational(3,2);System.out.println("""c  : week3.Rational = """ + $show(c ));$skip(29); val res$6 = 
   a.add(b).mul(c).toStringV2;System.out.println("""res6: String = """ + $show(res$6));$skip(29); val res$7 = 
-  a.sub(b).sub(c).toStringV2;System.out.println("""res7: String = """ + $show(res$7))}
+  a.sub(b).sub(c).toStringV2;System.out.println("""res7: String = """ + $show(res$7));$skip(25); val res$8 = 
+  
+  b.add(b).toStringV2;System.out.println("""res8: String = """ + $show(res$8));$skip(12); val res$9 = 
+  b.less(a);System.out.println("""res9: Boolean = """ + $show(res$9));$skip(22); val res$10 = 
+  b.max(a).toStringV2;System.out.println("""res10: String = """ + $show(res$10));$skip(69); 
+  
+  
+  //var denzero = new Rational(1,0)
+
+
+	var z = new Rational(9);System.out.println("""z  : week3.Rational = """ + $show(z ));$skip(14); val res$11 = 
+	z.toStringV2;System.out.println("""res11: String = """ + $show(res$11))}
 }
 
 class Rational(x: Int, y: Int) {
-	def numer = x
-	def denom = y
+	require(y != 0, "Denominator must be nonzero")
+
+	private def gcd(a: Int, b: Int): Int = {
+		if (b == 0) a
+		else gcd(b, a % b)
+	}
+	private val g: Int = gcd(x,y)
+
+	//set as constructor in case only one value is given
+	def this(x: Int) = this(x, 1)
+
+	def numer: Int = x / g
+	def denom: Int = y / g
+
+	def less(that: Rational): Boolean = {
+		numer * that.denom < denom * that.numer
+	}
+
+	def max(that: Rational): Rational = {
+	  if (this.less(that)) that
+		else this
+	}
 
 	def neg: Rational = new Rational(-numer, denom)
 	
@@ -54,5 +82,5 @@ class Rational(x: Int, y: Int) {
 		that.numer + "/" + that.denom
 	}
 	
-	def toStringV2() = numer + "/" + denom
+	def toStringV2: String = numer + "/" + denom
 }
--- src/week3/rationals.sc
+++ src/week3/rationals.sc
@@ -1,10 +1,7 @@
 package week3
 
 object rationals {
-  val x = new Rational(1, 2)                      //> x  : week3.Rational = week3.Rational@1a86f2f1
-  x.numer                                         //> res0: Int = 1
-  x.denom                                         //> res1: Int = 2
-  
+
   def addRational(a: Rational, b: Rational): Rational = {
   	new Rational((a.numer * b.denom) + (a.denom * b.numer), a.denom * b.denom)
   }                                               //> addRational: (a: week3.Rational, b: week3.Rational)week3.Rational
@@ -14,27 +11,58 @@ object rationals {
   }                                               //> makeString: (a: week3.Rational)String
   
   var s = addRational(new Rational(1, 2), new Rational(1, 2))
-                                                  //> s  : week3.Rational = week3.Rational@6bdf28bb
-  s.numer                                         //> res2: Int = 4
-  s.denom                                         //> res3: Int = 4
+                                                  //> s  : week3.Rational = week3.Rational@6b71769e
+  s.numer                                         //> res0: Int = 1
+  s.denom                                         //> res1: Int = 1
   makeString(addRational(new Rational(1, 2), new Rational(2, 3)))
-                                                  //> res4: String = 7/6
-                                                  
-                                                  
-  x.add(new Rational(2, 3)).toStringV2()          //> res5: String = 7/6
+                                                  //> res2: String = 7/6
+
+  val x = new Rational(1, 2)                      //> x  : week3.Rational = week3.Rational@1d251891
+  x.numer                                         //> res3: Int = 1
+  x.denom                                         //> res4: Int = 2
+  x.add(new Rational(2, 3)).toStringV2            //> res5: String = 7/6
   
-  
-  
-  val a = new Rational(1,3)                       //> a  : week3.Rational = week3.Rational@27f674d
-  val b = new Rational(5,7)                       //> b  : week3.Rational = week3.Rational@1d251891
-  val c = new Rational(3,2)                       //> c  : week3.Rational = week3.Rational@48140564
-  a.add(b).mul(c).toStringV2                      //> res6: String = 66/42
+  val a = new Rational(1,3)                       //> a  : week3.Rational = week3.Rational@48140564
+  val b = new Rational(5,7)                       //> b  : week3.Rational = week3.Rational@58ceff1
+  val c = new Rational(3,2)                       //> c  : week3.Rational = week3.Rational@7c30a502
+  a.add(b).mul(c).toStringV2                      //> res6: String = 11/7
   a.sub(b).sub(c).toStringV2                      //> res7: String = -79/42
+  
+  b.add(b).toStringV2                             //> res8: String = 10/7
+  b.less(a)                                       //> res9: Boolean = false
+  b.max(a).toStringV2                             //> res10: String = 5/7
+  
+  
+  //var denzero = new Rational(1,0)
+
+
+	var z = new Rational(9)                   //> z  : week3.Rational = week3.Rational@49e4cb85
+	z.toStringV2                              //> res11: String = 9/1
 }
 
 class Rational(x: Int, y: Int) {
-	def numer = x
-	def denom = y
+	require(y != 0, "Denominator must be nonzero")
+
+	private def gcd(a: Int, b: Int): Int = {
+		if (b == 0) a
+		else gcd(b, a % b)
+	}
+	private val g: Int = gcd(x,y)
+
+	//set as constructor in case only one value is given
+	def this(x: Int) = this(x, 1)
+
+	def numer: Int = x / g
+	def denom: Int = y / g
+
+	def less(that: Rational): Boolean = {
+		numer * that.denom < denom * that.numer
+	}
+
+	def max(that: Rational): Rational = {
+	  if (this.less(that)) that
+		else this
+	}
 
 	def neg: Rational = new Rational(-numer, denom)
 	
@@ -56,5 +84,5 @@ class Rational(x: Int, y: Int) {
 		that.numer + "/" + that.denom
 	}
 	
-	def toStringV2() = numer + "/" + denom
+	def toStringV2: String = numer + "/" + denom
 }
